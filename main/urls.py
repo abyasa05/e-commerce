62faba050c69b31526c22f44cf132b253c3e3c29
abyasa05 <abyprat07@gmail.com>
2024-09-16 21:21:28 +0700

    added form entry

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_shop_entry
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main')
+    path('', show_main, name='show_main'),
+    path('create_shop_entry', create_shop_entry, name='create_shop_entry'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ShopEntryForm
+from main.models import ShopEntry
 
 # Create your views here.
 def show_main(request):
-    return render(request, "main.html")
+    shop_entries = ShopEntry.objects.all()
+
+    context = {
+        'name': 'Muhammad Abyasa Pratama',
+        'class': 'PBP F',
+        'shop_entries': shop_entries
+    }
+    return render(request, "main.html", context)
+
+def create_shop_entry(request):
+    form = ShopEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+    
+    context = {'form': form}
+    return render(request, "create_shop_entry.html", context)
